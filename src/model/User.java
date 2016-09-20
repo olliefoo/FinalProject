d98de210338e278cb99a140242f530a1da0d9146
Ollie Foo <olliefoo@yahoo.com>
2016-09-20 15:10:38 -0400

    added more files for inital setup

---
+++ src/model/User.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ * Created by Owner on 9/20/2016.
+ */
+public class User {
+    private String name;
+    private String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
