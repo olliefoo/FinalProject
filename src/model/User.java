be734cb1bf6f7e2bc88e8cfc81dba90635887ab3
Ollie Foo <olliefoo@yahoo.com>
2016-10-02 18:16:33 -0400

    added data, getters, setters for the User class

--- src/model/User.java
+++ src/model/User.java
@@ -1,14 +1,71 @@
 package model;
 
 /**
- * Created by Owner on 9/20/2016.
+ * Created by Ollie Foo on 9/20/2016.
  */
 public class User {
-    private String name;
+    private String username;
     private String password;
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String address;
+    private String phone;
+    private String gender;
+    private String birthday;
 
-    public User(String name, String password) {
-        this.name = name;
+    public User(String username, String password, String email) {
+        this.username = username;
         this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String fn, String ln) {
+        if (!firstname.equals(fn)) {
+            firstname = fn;
+        }
+        if (!(lastname.equals(ln))) {
+            lastname = fn;
+        }
+    }
+
+    public void setEmail(String s) {
+        email = s;
+    }
+
+    public void setAddress(String street, String state, String city, String zip) {
+        address = street + ", " + state + ", " + city + ", " + zip;
+    }
+
+    public void setPhone(String p) {
+        phone = p;
+    }
+
+    public void setGender(String g) {
+        gender = g;
+    }
+
+    public void setBirthday(String month, String day, String year) {
+        birthday = month + "/" + day + "/" + year;
+    }
+
+    public boolean setPassword(String p) {
+        if (password.equals(p)) {
+            return false;
+        }
+        password = p;
+        return true;
     }
 }
