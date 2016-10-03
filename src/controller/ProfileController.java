636cd96665df7bb0c51f6c4a1207a625da5b7816
MilesJ150 <miles.johnson1996@gmail.com>
2016-10-02 23:40:52 -0400

    modified comboboxes

--- src/controller/ProfileController.java
+++ src/controller/ProfileController.java
@@ -69,8 +69,16 @@ public class ProfileController {
         zipField.setText(user.getZip());
 
         monthField.getItems().addAll(
-                "January", "February", "March", "April", "May", "June", "July",
-                "August", "September", "October", "November", "December"
+                "Jan", "Feb", "Mar", "Apr", "May", "June", "July",
+                "Aug", "Sept", "Oct", "Nov", "Dec"
+        );
+
+        dayField.getItems().addAll(
+                "1", "2", "3", "4", "5", "6", "7",
+                "8", "9", "10", "11", "12", "13", "14",
+                "15", "16", "17", "18", "19", "20", "21",
+                "22", "23", "24", "25", "26", "27", "28",
+                "29", "30", "31", "32"
         );
     }
 
