08e79bb1eda0fc78a20282d557efeb4ad82750eb
cbbjs <bkim360@gatech.edu>
2016-10-02 20:21:29 -0400

    Linked appStartController to the profile screen when profile button is pressed.

--- src/controller/AppStartController.java
+++ src/controller/AppStartController.java
@@ -15,9 +15,20 @@ public class AppStartController {
     private Button myLogout;
 
     @FXML
+    private Button profile;
+
+    @FXML
     private void handleLogoutPressed() throws IOException {
         Stage stage = (Stage) myLogout.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("../view/WelcomeScreen.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    @FXML
+    private void handleProfilePressed() throws IOException {
+        Stage stage = (Stage) profile.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("../view/ProfileScreen.fxml"));
         stage.setScene(new Scene(root));
         stage.show();
     }
