40417c57ef0d166fe462364f19e6fa911ff8b089
kijanaeffs <k.effs95@gmail.com>
2016-11-15 04:22:05 -0500

    Kijana junit

--- src/test/KijanaTest.java
+++ src/test/KijanaTest.java
@@ -1,7 +1,55 @@
 package test;
+import model.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
- * Created by Kijana on 11/15/2016.
+ * Created by Kijana.
  */
 public class KijanaTest {
+
+    @Test
+    public void addAdmin() {
+        String username = "admin";
+        String password = "adminP";
+        String email = "adminE";
+        Admin admin = new Admin(username, password, email);
+        Database db = new Database();
+        db.add(admin);
+        assertEquals(admin, db.getAdmin(username));
+    }
+
+    @Test
+    public void addManager() {
+        String username = "man";
+        String password = "manP";
+        String email = "manE";
+        Manager man = new Manager(username, password, email);
+        Database db = new Database();
+        db.add(man);
+        assertEquals(man, db.getManager(username));
+    }
+
+    @Test
+    public void addWorker() {
+        String username = "worker";
+        String password = "workerP";
+        String email = "workerE";
+        Worker work = new Worker(username, password, email);
+        Database db = new Database();
+        db.add(work);
+        assertEquals(work, db.getWorker(username));
+    }
+
+    @Test
+    public void addUser() {
+        String username = "user";
+        String password = "userP";
+        String email = "userE";
+        User user = new User(username, password, email);
+        Database db = new Database();
+        db.add(user);
+        assertEquals(user, db.getUser(username));
+    }
 }
+
