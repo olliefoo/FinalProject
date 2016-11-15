a46f6c333a06a73bc5f0b99fbb452b79a998ce19
Ollie Foo <olliefoo@yahoo.com>
2016-11-15 03:22:53 -0500

    junit test for getUser() in Database

--- src/test/OllieTest.java
+++ src/test/OllieTest.java
@@ -1,13 +1,23 @@
 package test;
 
+import model.Database;
+import model.User;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Created by Owner on 11/14/2016.
  */
 public class OllieTest {
     @Test
-    public void test() {
+    public void testGetUser() {
+        String username = "Ollie";
+        String password = "password";
+        String email = "ofoo3@gatech.edu";
+        User user = new User(username, password, email);
 
+        Database database = new Database();
+        Database.add(user);
+        assertEquals(user, Database.getUser(username));
     }
 }
